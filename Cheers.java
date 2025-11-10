b87dcde65c9060bdf5f2e48109873e41896ecc69
Shiraz Yoel <shiraz.jaul@post.runi.ac.il>
2025-11-10 13:17:54 +0200

    f

--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,27 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+        String cheering = (args [0]);
+        int x = Integer.parseInt(args [1]);
+        String m = "EFHILMNORSX";
+        int i = 0;
+        while (i < cheering.length()){
+                char c = cheering.charAt(i);
+                if (m.indexOf(c) != -1){
+                        System.out.println("give me an " + c + ": " + c + "!");
+                }
+                else {
+                        System.out.println("give me a " + c + ": " + c + "!");
+                }
+                i++;
+
+         }
+        
+        while (x > 0) {
+                System.out.println(cheering + "!");
+                x--;
+                        
+                }
         }
-}
+        }
+
